3a6ee9b3aaf1a8ab69f43180cdba89157b9dc8cf
aditya-adiraju <adiraju@student.ubc.ca>
2023-11-01 00:29:16 -0700

    Okay I actually I get how to make a symlink (sorta)

---
+++ makeSymlinks.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+
+# remove exisiting files
+sudo rm /etc/nixos/configuration.nix
+sudo rm /etc/nixos/hardware-configuration.nix
+
+# Create symlinks for nixos from PWD to their respective locations
+sudo ln -s $PWD/nix/configuration.nix /etc/nixos/configuration.nix
+sudo ln -s $PWD/nix/hardware-configuration.nix /etc/nixos/hardware-configuration.nix
+
