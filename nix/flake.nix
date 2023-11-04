1b26fd55261c4e1d90385f4858ce621a81a542f0
aditya-adiraju <adiraju@student.ubc.ca>
2023-11-03 22:24:13 -0700

    Fix: flake and lock file

--- nix/flake.nix
+++ nix/flake.nix
@@ -4,7 +4,7 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs }: {
-    nixosConfigurations.container = nixpkgs.lib.nixosSystem {
+    nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [ ./configuration.nix ];
     };
