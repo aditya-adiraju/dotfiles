6b3275518dd03aec9bf81d13a8a153b1ccadaffd
aditya-adiraju <adiraju@student.ubc.ca>
2024-05-02 22:18:33 -0700

    Updated lock and added FHS shortcut

--- nix/packages.nix
+++ nix/packages.nix
@@ -7,7 +7,7 @@
     git
     gh
     gdb
-    #pwndbg
+    pwndbg
     tilix
     tmux
     nix-ld
@@ -60,7 +60,27 @@
 
     sage
     osu-lazer-bin
-    poetry
+
+    # An FHS shortcut to help us from the interwebs
+    (let base = pkgs.appimageTools.defaultFhsEnvArgs; in
+          pkgs.buildFHSUserEnv (base // {
+          name = "fhs";
+          targetPkgs = pkgs: (
+            # pkgs.buildFHSUserEnv provides only a minimal FHS environment,
+            # lacking many basic packages needed by most software.
+            # Therefore, we need to add them manually.
+            #
+            # pkgs.appimageTools provides basic packages required by most software.
+            (base.targetPkgs pkgs) ++ [
+              pkgs.pkg-config
+              pkgs.ncurses
+              # Feel free to add more packages here if needed.
+            ]
+          );
+          profile = "export FHS=1";
+          runScript = "bash";
+          extraOutputsToInstall = ["dev"];
+        }))
   ] ++ 
   [
     #pkgs.cachix
