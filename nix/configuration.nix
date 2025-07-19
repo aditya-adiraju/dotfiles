2700a41e91263eafd80e354c95cb7e44ccca9e85
aditya-adiraju <adiraju@student.ubc.ca>
2025-07-18 17:34:22 -0700

    Add Ulauncher and add pretty terminals

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -44,12 +44,12 @@
 
   # Enable Hyprland
   #programs.hyprland.enable = true;
-  services.desktopManager.plasma6.enable = true;
-  services.displayManager.sddm.enable = true;
-  services.displayManager.sddm.wayland.enable = true;
+  # services.desktopManager.plasma6.enable = true;
+  # services.displayManager.sddm.enable = true;
+  # services.displayManager.sddm.wayland.enable = true;
   # Enable the GNOME Desktop Environment.
-  # services.xserver.displayManager.gdm.enable = true;
-  # services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   # docker support
   virtualisation.docker.enable = true;
